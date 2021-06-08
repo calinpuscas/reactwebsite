e1dca302f2116ea7247781c5f44b536a09bebc60
calinpuscas <84851565+calinpuscas@users.noreply.github.com>
2021-06-08 16:17:59 +0300

    Update ServicesElements.js

--- src/components/Services/ServicesElements.js
+++ src/components/Services/ServicesElements.js
@@ -78,7 +78,7 @@ export const ServicesIcon = styled.img`
 `;
 
 export const ServicesH1 = styled.h1`
-  font-size: 2.5rem;
+  font-size: 2rem;
   color: #007873;
   font-weight: 900;
   margin-bottom: 24px;
@@ -90,7 +90,7 @@ export const ServicesH1 = styled.h1`
 `;
 
 export const ServicesH2 = styled.h2`
-  font-size: 1.5rem;
+  font-size: 3.5rem;
   font-weight: bold;
   margin-bottom: 10px;
   color: #f9f1e7;
